de1133ab4377c54b410b768517aa7dec8e816f62
soyalk1 <45288561+soyalk1@users.noreply.github.com>
2018-12-06 21:29:09 +0530

    Update Soyalk.py

--- Soyalk.py
+++ Soyalk.py
@@ -12,7 +12,7 @@ import time
 import datetime
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("$"), pm_help = True)
+client = commands.Bot(description="Here is some command for you", command_prefix=commands.when_mentioned_or("$"), pm_help = False)
 
 
 async def status_task():
