b6cc81f8db88d3fd1392b83664b50e258b3c73c9
yash9879 <48157388+yash9879@users.noreply.github.com>
2019-03-02 20:32:48 +0530

    Update Soyalk.py

--- Soyalk.py
+++ Soyalk.py
@@ -23,7 +23,7 @@ async def status_task():
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
         await asyncio.sleep(5)
-@client.event
+
 async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
